2cb53d2467355990407ed8cf6d5bb5df83e6739c
PolinaSavelyeva <lissava00@gmail.com>
2023-09-15 23:26:21 +0300

    Add new PackageId and update docs

--- README.md
+++ README.md
@@ -27,7 +27,7 @@ or
 Go to directory with your ``build.fsproj`` (or ``build.csproj``) file and install ImageProcessing using command line:
 
 ```shell
-dotnet add package ImageProcessingByPolinaSavelyeva --version 1.0.0
+dotnet add package ImageProcessing.PolinaSavelyeva --version 1.0.0
 ```
 
 For more information visit package main [GitHub page](https://github.com/PolinaSavelyeva/ImageProcessing/pkgs/nuget/ImageProcessing).
--- build/build.fs
+++ build/build.fs
@@ -35,8 +35,6 @@ let rootDirectory =
     __SOURCE_DIRECTORY__
     </> ".."
 
-let packageName = "ImageProcessingByPolinaSavelyeva"
-
 let productName = "ImageProcessing"
 let sln = __SOURCE_DIRECTORY__ </> ".." </> "ImageProcessing.sln"
 
@@ -579,7 +577,7 @@ let createPackages _ =
                 sprintf "/p:RuntimeIdentifier=%s" runtime
                 sprintf "/p:Configuration=%s" "Release"
                 sprintf "/p:PackageVersion=%s" latestEntry.NuGetVersion
-                sprintf "/p:PackagePath=\"%s\"" (distDir @@ (sprintf "%s-%s-%s" packageName latestEntry.NuGetVersion runtime ))
+                sprintf "/p:PackagePath=\"%s\"" (distDir @@ (sprintf "%s-%s-%s" productName latestEntry.NuGetVersion runtime ))
             ] |> String.concat " "
 
         DotNet.exec (fun opt ->
